a0f6dedbaad3956d0391f307971fb5327577d6a8
Ramatoulaye CAMARA <camara.ramatoulaye1@ugb.edu.sn>
2025-01-09 11:23:32 +0000

    last commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { EnteteComponent } from "./frame/entete/entete.component";
+import { MenuComponent } from "./frame/menu/menu.component";
+import { AccueilComponent } from "./accueil/accueil.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, EnteteComponent, MenuComponent, AccueilComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
